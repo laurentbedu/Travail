39a761c78bdf82db28dbca587635c57a53ae6155
Thierry BROUET <56256915+thierryDev62@users.noreply.github.com>
2020-01-06 16:04:59 +0100

    update

--- lacr-immo/src/js/accueil.js
+++ lacr-immo/src/js/accueil.js
@@ -1,3 +1,16 @@
 $(function(){
-    $("body").css("font-family", "'Poppins', Arial, sans-serif");
+    // Initialisation CSS Body
+    $("body").css({"font-family": "'Poppins', Arial, sans-serif", "overflow-x": "hidden"});
+
+    // Méthodes opacité sur "Agence au passage de la souris"
+    $(".modalAgency").on("mouseover", function(){
+    let style = {
+        cursor: "pointer",
+        opacity: 0.6
+    };
+        $(this).css(style);
+    });
+    $(".modalAgency").on("mouseleave", function(){
+        $(".modalAgency").css("opacity", "1");
+    });
 });
